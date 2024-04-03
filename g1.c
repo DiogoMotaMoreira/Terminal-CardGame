b24e9a5e3f8c532556dded84ff482475d40b913f
Diogo_Moreira_95 <a106841@alunos.uminho.pt>
2024-04-03 22:09:34 +0100

    f1 - conjuntos

--- g1.c
+++ g1.c
@@ -28,3 +28,18 @@ typedef enum {
     REI,
 
 } VALOR;
+
+int f1(wchar_t x[]){
+    VALOR v = valor(x[0]);
+    int count = 0;
+
+    // ciclo for com funções valor e naipe feitas por DANI
+    for(int i = 0; x[i] != '\0'; i++)
+    {
+        if(valor(x[i])==v) count++;
+        else return 0;
+    }
+
+    return count;
+}
+
