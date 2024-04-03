cd8a79fcf38b0f9df8cd17f8bd4403bd4ae24106
DanielPereira212 <a106912@alunos.uminho.pt>
2024-04-03 21:08:07 +0100

    defenição de tipos VALOR NAIPE

---
+++ teste.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <math.h>
+#include <string.h>
+
+typedef enum {
+    PAUS,
+    OUROS,
+    COPAS,
+    ESPADAS,
+
+} NAIPE;
+
+typedef enum {
+    AS,
+    DOIS,
+    TRES,
+    QUATRO,
+    CINCO,
+    SEIS,
+    SETE,
+    OITO,
+    NOVE,
+    DEZ,
+    VALETE,
+    C,
+    DAMA,
+    REI,
+
+} VALOR;
