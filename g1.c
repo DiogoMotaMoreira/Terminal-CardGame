4d3bb7b81652ee6faa55272716be028fb378f562
DanielPereira212 <a106912@alunos.uminho.pt>
2024-04-05 16:57:38 +0100

    adicao funcoes valor e naipe

--- g1.c
+++ g1.c
@@ -29,6 +29,28 @@ typedef enum {
 
 } VALOR;
 
+NAIPE naipe (wchar_t carta) {
+    NAIPE resultado = 0;
+    if (carta >= 0x1F0A1 && carta <= 0x1F0AE) resultado = ESPADAS;
+    else if (carta >= 0x1F0B1 && carta <= 0x1F0BE) resultado = COPAS;
+    else if (carta >= 0x1F0C1 && carta <= 0x1F0CE) resultado = OUROS;
+    else resultado = PAUS;
+
+    return resultado;
+}
+
+VALOR valor (wchar_t carta){
+    VALOR resultado = 0;
+    wchar_t i;
+    int j = 0;
+    if (naipe(carta) == ESPADAS) for (i = 0x1F0A1; i<= 0x1F0AE; i++) {if (carta == i) resultado = j; j++;}
+    else if (naipe(carta) == COPAS) for (i = 0x1F0B1; i<= 0x1F0BE ; i++) {if (carta == i) resultado = j; j++;}
+    else if (naipe(carta) == OUROS) for (i = 0x1F0C1; i<= 0x1F0CE; i++) {if (carta == i) resultado = j; j++;}
+    else for (i = 0x1F0D1; i<= 0x1F0DE; i++) {if (carta == i) resultado = j; j++;}
+
+    return resultado;
+}
+
 int f1(wchar_t x[]){
     VALOR v = valor(x[0]);
     int count = 0;
